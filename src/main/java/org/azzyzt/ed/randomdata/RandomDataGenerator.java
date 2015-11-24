f57be0e077a2dde38b2f15d8e7f3b9d9a8e0ff83
Andreas <andreas@manessinger.com>
2015-11-24 22:25:05 +0100

    Initial commit

---
+++ src/main/java/org/azzyzt/ed/randomdata/RandomDataGenerator.java
@@ -0,0 +1,9 @@
+package org.azzyzt.ed.randomdata;
+
+public class RandomDataGenerator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/azzyzt/ed/randomdata/RandomDataGeneratorTest.java
@@ -0,0 +1,15 @@
+package org.azzyzt.ed.randomdata;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+ 
+/**
+ * Unit tests for RandomDataGenerator.
+ */
+public class RandomDataGeneratorTest
+{
+    @Test
+    public void aDummyTest() {
+        Assert.assertTrue(true);
+    }
+}
