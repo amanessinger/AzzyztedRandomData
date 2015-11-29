33df6ddf94d3eee54610ec89efcb90d8e601b0b0
Andreas <andreas@manessinger.com>
2015-11-29 13:12:54 +0100

    Basic structure is set up, no randomness yet, no data sources

--- src/main/java/org/azzyzt/ed/randomdata/RandomDataGenerator.java
+++ src/main/java/org/azzyzt/ed/randomdata/RandomDataGenerator.java
@@ -1,9 +1,51 @@
 package org.azzyzt.ed.randomdata;
 
-public class RandomDataGenerator 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomDataGenerator {
+
+    private static class ArgsProcessor {
+
+       int requestedNumber;
+
+       public boolean argumentsOk(String[] args) {
+            if (args == null || args.length != 1) {
+                return false;
+            }
+            String reqNumArg = args[0];
+            try {
+                requestedNumber = Integer.parseUnsignedInt(reqNumArg);
+            }
+            catch (NumberFormatException e) {
+                System.err.println(reqNumArg + " is not an unsigned integer");
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        ArgsProcessor a = new ArgsProcessor();
+        if (!a.argumentsOk(args)) {
+            return;
+        }
+
+        RandomDataGenerator rdg = new RandomDataGenerator();
+        rdg.randomStrings(a.requestedNumber).forEach(System.out::println);
+    }
+
+    public RandomDataGenerator() {
+        // TODO add some kind of initializations with tagged data sources
+    }
+
+    public List<String> randomStrings(int number) {
+        List<String> res = new ArrayList<>();
+
+        res.add("RANDOMNESS NOT YET IMPLEMENTED"); // TODO make this random and depend on number
+
+        return res;
     }
 }
--- src/test/java/org/azzyzt/ed/randomdata/RandomDataGeneratorTest.java
+++ src/test/java/org/azzyzt/ed/randomdata/RandomDataGeneratorTest.java
@@ -2,14 +2,25 @@ package org.azzyzt.ed.randomdata;
 
 import org.testng.Assert;
 import org.testng.annotations.*;
- 
+
+import java.util.List;
+
 /**
  * Unit tests for RandomDataGenerator.
  */
 public class RandomDataGeneratorTest
 {
+    private RandomDataGenerator rdg;
+
+    @BeforeMethod
+    public void setupGenerator(){
+        rdg = new RandomDataGenerator();
+    }
+
     @Test
-    public void aDummyTest() {
-        Assert.assertTrue(true);
+    public void returnsOneRandomString() {
+        final List<String> strings = rdg.randomStrings(1);
+        Assert.assertNotNull(strings);
+        Assert.assertEquals(strings.size(), 1);
     }
 }
